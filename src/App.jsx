c8de692231f7bcf62685864c4b82b5f5f27b398a
ChristianRiovaldo <christian.riovaldo10@gmail.com>
2025-02-16 21:54:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Board from "./components/Board.jsx";
+
+function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  function handlePlay(newSquares) {
+    const newHistory = [...history.slice(0, currentMove + 1), newSquares];
+    setHistory(newHistory);
+    setCurrentMove(newHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  const moves = history.map((squares, move) => {
+    let text = "";
+    if (move > 0) {
+      text = "Go to move #" + move;
+    } else {
+      text = "Go to game start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{text}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
